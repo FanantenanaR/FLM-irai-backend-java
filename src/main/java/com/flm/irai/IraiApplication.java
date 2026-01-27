6dd3fa67c1bd50de4ee27690e935f76ff8ae2741
Jason Andriniaina RALIJAONA <jasonralijaona@gmail.com>
2026-01-27 13:56:58 +0300

    initialisation du projet backend java en spring boot

---
+++ src/main/java/com/flm/irai/IraiApplication.java
@@ -0,0 +1,13 @@
+package com.flm.irai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IraiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flm/irai/IraiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flm.irai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IraiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
